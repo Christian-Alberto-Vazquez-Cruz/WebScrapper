cf31b6659fa09baae7dd0702c8f8a155f441812b
Christian Alberto Vázquez Cruz <zS22013636@estudiantes.uv.mx>
2025-02-28 00:26:41 -0600

    Browing enabled

---
+++ MarketPlaceWebScraping/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class Program() {
+    static void Main() {
+        ChromeOptions options = new ChromeOptions();
+        options.AddArguments("start-maximized");
+        IWebDriver driver = new ChromeDriver(options);
+
+        driver.Navigate().GoToUrl("http://www.facebook.com");
+        Thread.Sleep(5000);
+
+    }
+}
